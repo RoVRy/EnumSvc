d0ffe3b71e5cacebce04ab53a8c772b36b5a09ff
Rex2701 <58503026+Rex2701@users.noreply.github.com>
2019-12-04 21:06:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.ServiceProcess;
+using System.Configuration;
+
+namespace EnumSvc
+{
+    class Program
+    {
+        class CurrentService
+        {
+            public string ServiceName;
+            public string DisplayName;
+            public string Status;
+        }
+
+        static void Main(string[] args)
+        {
+            CurrentService CurSvc = new CurrentService();
+            ServiceController[] ComSvc,DevSvc;
+            ComSvc = ServiceController.GetServices();
+            DevSvc = ServiceController.GetDevices();
+            int CSc = 0, DSc = 0, CSl = ComSvc.Length, DSl = DevSvc.Length,MinNumOfSvc = (CSl > DSl) ? DSl : CSl;
+            while (CSc < CSl && DSc < DSl)
+            {
+                if (String.Compare(ComSvc[CSc].ServiceName,DevSvc[DSc].ServiceName,true)>0)
+                {
+                    CurSvc.ServiceName = DevSvc[DSc].ServiceName;
+                    CurSvc.DisplayName = DevSvc[DSc].DisplayName;
+                    CurSvc.Status = DevSvc[DSc].Status.ToString();
+                    DSc++;
+                }
+                else
+                {
+                    CurSvc.ServiceName = ComSvc[CSc].ServiceName;
+                    CurSvc.DisplayName = ComSvc[CSc].DisplayName;
+                    CurSvc.Status = ComSvc[CSc].Status.ToString();
+                    CSc++;
+                }
+                Console.Write(CurSvc.ServiceName); Console.Write('\t');
+                Console.Write(CurSvc.DisplayName); Console.Write('\t');
+                Console.WriteLine(CurSvc.Status);
+            }
+            if (DSc == DSl)
+               while (++CSc < CSl)
+               {
+                    Console.Write(ComSvc[CSc].ServiceName); Console.Write('\t');
+                    Console.Write(ComSvc[CSc].DisplayName); Console.Write('\t');
+                    Console.WriteLine(ComSvc[CSc].Status.ToString());
+                    
+               }
+            else while (++DSc < DSl)
+                {
+                    Console.Write(DevSvc[DSc].ServiceName); Console.Write('\t');
+                    Console.Write(DevSvc[DSc].DisplayName); Console.Write('\t');
+                    Console.WriteLine(DevSvc[DSc].Status.ToString());
+                }
+            }
+        }
+    }
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EnumSvc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ООО ЗСМ")]
+[assembly: AssemblyProduct("EnumSvc")]
+[assembly: AssemblyCopyright("Copyright © ООО ЗСМ 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e54a57a-cf11-4e65-b91e-7e00c2dae38a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
